12fdc71186a1896dea4c049edb2fce5ebdd72171
Istvan Kekenj <fajnl.fentazi.7@gmail.com>
2014-12-13 16:29:14 +0100

    iso zadatak, prva ideja, brute force

---
+++ junior/iso6801/index.php
@@ -0,0 +1,17 @@
+<?php
+    $s = "10#51#16";
+    switch ($s) {
+        case $s countains '#':
+            echo "format je sa #";
+            break;
+        case $s contains '/':
+            echo "format je sa /";
+            break;
+        case $s contains '-':
+            echo "format je sa -";
+            break;
+        case $s contains '*':
+            echo "format je sa *";
+            break;
+    }
+?>
