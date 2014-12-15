ee260d720c6904b594b05fb10e133a1d018e9c86
Istvan Kekenj <fajnl.fentazi.7@gmail.com>
2014-12-15 01:15:09 +0100

    jednostavnije resenje

--- junior/iso6801/index.php
+++ junior/iso6801/index.php
@@ -5,67 +5,16 @@
         $str = fgets($file);
         if (strpos($str, '#') !== false) {
             if (substr($str, 3) >= "14") {
-                $date = "19" . substr($str, 3, 2) . "-" . substr($str, 6, 2) . "-" . substr($str, 0, 2);
+                $str = "19" . substr($str, 3, 2) . "-" . substr($str, 6, 2) . "-" . substr($str, 0, 2);
             } else {
-                $date = "20" . substr($str, 3, 2) . "-" . substr($str, 6, 2) . "-" . substr($str, 0, 2);
+                $str = "20" . substr($str, 3, 2) . "-" . substr($str, 6, 2) . "-" . substr($str, 0, 2);
             }
-        } elseif (strpos($str, '/') !== false) {
-            if (substr($str, 6) >= "14") {
-                $date = "19" . substr($str, 6, 2) . "-" . substr($str, 0, 2) . "-" . substr($str, 3, 2);
-            } else {
-                $date = "20" . substr($str, 6, 2) . "-" . substr($str, 0, 2) . "-" . substr($str, 3, 2);
-            }
-        } elseif (strpos($str, ' ') !== false) {
-            switch (substr($str, 0, 3)) {
-                case "Jan" :
-                    $month = "01";
-                    break;
-                case "Feb" :
-                    $month = "02";
-                    break;
-                case "Mar" :
-                    $month = "03";
-                    break;
-                case "Apr" :
-                    $month = "04";
-                    break;
-                case "May" :
-                    $month = "05";
-                    break;
-                case "Jun" :
-                    $month = "06";
-                    break;
-                case "Jul" :
-                    $month = "07";
-                    break;
-                case "Aug" :
-                    $month = "08";
-                    break;
-                case "Sep" :
-                    $month = "09";
-                    break;
-                case "Oct" :
-                    $month = "10";
-                    break;
-                case "Nov" :
-                    $month = "11";
-                    break;
-                case "Dec" :
-                    $month = "12";
-                    break;
-            }
-            if (substr($str, 8) >= "14") {
-                $date = "19" . substr($str, 8, 2) . "-" . $month . "-" . substr($str, 4, 2);
-            } else {
-                $date = "20" . substr($str, 8, 2) . "-" . $month . "-" . substr($str, 4, 2);
-            }
-        } elseif (strpos($str, '*') !== false) {
-            $date = trim(substr($str, 6)) . "-" . substr($str, 3, 2) . "-" . substr($str, 0, 2);
         } else {
-            $date = trim($str);
+            $str = str_replace('*', '-', $str);
+            $str = date("Y-m-d", strtotime($str));
         }
-        trim($date);
-        fwrite($output, $date . PHP_EOL);
+        trim($str);
+        fwrite($output, $str . PHP_EOL);
     }
     fclose($file);
     fclose($output);
